b921197a14e624d82cdf0f63a160cd80bbbdc0c2
Phssthp0k <43260861+Phssthp0k@users.noreply.github.com>
2018-09-14 06:53:29 +0200

    First Upload
    First revision with classes and GameClock (general Interval)
    Must be changed to classes internal Interval.

---
+++ js/score.js
@@ -0,0 +1,28 @@
+class scoringProto 
+{
+	constructor()
+	{
+		this.lifes = 3;
+		this.score = 0;
+		this.clicks = { 
+						lastClickTime : new Date().getTime(), 
+						lastClickDuration : 0, 
+						bestClickDuration : 1000, 
+						clicksAmount : 0, 
+						totalPlayTime : 0,
+						avgClickDuration : 0
+					}; 
+		this.avgClicksPerMinute = 0;
+	}
+	
+	peek()
+	{
+		return 5;
+	}
+};
+
+function ResetScore()
+{
+	// Log( 'Resetting Score' );
+	scoring = new scoringProto();
+}
