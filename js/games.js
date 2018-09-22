a77b18683886ec7b7b33496300fc63b50ecce19d
phssthp0k <luca.marinosci@gmail.com>
2018-09-22 21:06:41 +0200

    Funziona, siamo al primo prototipo per quickAim, bisogna aggiungere solo la logica!

--- js/games.js
+++ js/games.js
@@ -70,6 +70,7 @@ class Game_Class
 		this._id = guid();
 		this._description = "";
 		this._ico = "something";
+		this._drawing_Class = drawing_Class;
 	}
 
 	get name() {return this._name;}
@@ -87,33 +88,50 @@ class Game_Class
 
 class QuickAim_Class extends Game_Class
 {
-	constructor(drawing_Class)
+	constructor(canvasManager_Class)
 	{
-		super(drawing_Class);
+		super(canvasManager_Class);
+
 		this.name = "Quick Aim!";
+		this.target = new Target_Class();
+		this.target2 = new Target_Class();
 	}
 	LoadGame()
 	{
 		super.LoadGame();
 
-		var target = new Target_Class();
-		target.name = "mainTarget";
-		target.SetCircles(3);
-		target.maxRadius = 80;
-		target.targetPosition = drawing.canvasCenter;
-		target.targetPosition = { x: ((Math.random() * drawing.Canvas.width) + 1), y: ((Math.random() * drawing.Canvas.height) + 1) };
+		this.target.name = "mainTarget";
+		this.target.SetCircles(3);
+		this.target.maxRadius = 80;
+		this.target.targetPosition = this._drawing_Class.canvasCenter;
+		this.target.targetPosition = { x: ((Math.random() * this._drawing_Class.Canvas.width) + 1), y: ((Math.random() * this._drawing_Class.Canvas.height) + 1) };
+		
+		this.target2.name = "target2";
+		this.target2.SetCircles(3);
+		this.target2.maxRadius = 40;
+		this.target2.targetPosition = this._drawing_Class.canvasCenter;
+		this.target2.targetPosition = { x: ((Math.random() * this._drawing_Class.Canvas.width) + 1), y: ((Math.random() * this._drawing_Class.Canvas.height) + 1) };
 
-		drawing.UpdateObjectList( [ target ] );
-		drawing.AddOnClickFunction( this.OnClick );
-		drawing.RefreshScreen();
-
+		this._drawing_Class.UpdateObjectList( [ this.target, this.target2 ] );
+		this._drawing_Class.AddOnClickFunction( this );
+		this._drawing_Class.RefreshScreen();
 		// mostro la scritta start e il primo target
 		// al click sul target parte il timer
 	}
 
-	OnClick()
+	OnClick(mouseEvents)
 	{
 		log("clicked");
+		if( this.target.IsHit(mouseEvents) > 0 )
+		{
+			this.target.targetPosition = { x: ((Math.random() * this._drawing_Class.Canvas.width) + 1), y: ((Math.random() * this._drawing_Class.Canvas.height) + 1) };
+		}
+		if( this.target2.IsHit(mouseEvents) > 0 )
+		{
+			this.target2.targetPosition = { x: ((Math.random() * this._drawing_Class.Canvas.width) + 1), y: ((Math.random() * this._drawing_Class.Canvas.height) + 1) };
+		}
+		this._drawing_Class.RefreshScreen();
+			
 	}
 }
 
